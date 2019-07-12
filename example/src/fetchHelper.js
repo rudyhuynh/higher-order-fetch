3ed538d2654b9cb56182a862db7d34c552dd2c76
rudyhuynh <rudyhuynh@niometrics.com>
2019-07-13 03:10:23 +0800

    remove comment

--- example/src/fetchHelper.js
+++ example/src/fetchHelper.js
@@ -14,7 +14,7 @@ const fetchHelper = {
   fetch: pipe(
     onErrorRetry(),
     defaultHeaders({
-      // "Content-Type": "application/json"
+      "Content-Type": "application/json"
     }),
     jsonResponse(),
     interceptor()
